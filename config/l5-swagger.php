7fb5001de1abdbe10b26725209244d8423c344ea
Papa Sidy Fall <papasidyfall03@gmail.com>
2025-10-30 14:43:15 +0000

    Add multiple servers configuration for Swagger (local and production)

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -6,6 +6,16 @@ return [
         'default' => [
             'api' => [
                 'title' => 'API Gestion Bancaire v1.0',
+                'servers' => [
+                    [
+                        'url' => 'http://localhost:8000',
+                        'description' => 'Serveur de développement local'
+                    ],
+                    [
+                        'url' => 'https://gestion-banquaire.onrender.com',
+                        'description' => 'Serveur de production'
+                    ]
+                ],
             ],
 
             'routes' => [
